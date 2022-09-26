7f4e6fdae043bd850b24b499c54d4b33a2481495
agung1997-mhs <agungkemayoran97@gmail.com>
2022-09-26 18:26:59 +0700

    menambahkan data di cms dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,11 +24,19 @@ Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('det
 Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
 Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
-Route::middleware(['auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+    Route::middleware(['admin'])->group(function() {
+        Route::resource('product', ProductController::class);
+        Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
+            'index', 'create', 'store', 'destroy'
+        ]);
+        Route::resource('transaction', TransactionController::class)->only([
+            'index', 'show', 'edit', 'update'
+        ]);
+        Route::resource('user', UserController::class)->only([
+            'index', 'edit', 'update', 'destroy'
+        ]);
+    });
 });
