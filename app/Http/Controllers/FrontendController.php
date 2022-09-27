e7969e1c434be1ade770d38a6522d1b2cd491cb6
agung1997-mhs <agungkemayoran97@gmail.com>
2022-09-27 15:23:08 +0700

    add to cart

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,23 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.frontend.index');
+        $products = Product::with(['galleries'])->latest()->get();
+
+        return view('pages.frontend.index', [
+            'products' => $products
+        ]);
     }
 
     public function details(Request $request, $slug)
     {
-        return view('pages.frontend.details');
+        $product = Product::with(['galleries'])->where('slug', $slug)->firstOrFail();
+        $recommendations = Product::with(['galleries'])->inRandomOrder()->limit(4)->get();
+
+        return view('pages.frontend.details', [
+            'product' => $product,
+            'recommendations' => $recommendations
+        ]);
+    }
+
+
+    public function cartAdd(Request $request, $id)
+    {
+        Cart::create([
+            'users_id' => Auth::user()->id,
+            'products_id' => $id
+        ]);
+
+        return redirect('cart');
+    }
+
+
+    public function cartDelete(Request $request, $id)
+    {
+        $item = Cart::findOrFail($id);
+
+        $item->delete();
+
+        return redirect('cart');
     }
 
     public function cart(Request $request)
     {
-        return view('pages.frontend.cart');
+        $carts = Cart::with(['product.galleries'])->where('users_id', Auth::user()->id)->get();
+
+        return view('pages.frontend.cart', [
+            'carts' => $carts
+        ]);
     }
 
     public function success(Request $request)
